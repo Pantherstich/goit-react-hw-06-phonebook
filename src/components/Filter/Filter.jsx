9adbc88511fa08ed0799933ef62fc8c466da1501
Pantherstich <132139666+Pantherstich@users.noreply.github.com>
2023-12-23 00:24:25 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,16 @@
 import { H2Title } from 'components/App.styled';
 import { FilterField } from './Filter.styled';
+import { getFilter } from '../../redux/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from '../../redux/filterSlice';
 
-export const Filter = ({ filter, onChangeFilter }) => {
+export const Filter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getFilter);
+
+  const onChangeFilter = e => {
+    dispatch(setFilter(e.target.value));
+  };
   return (
     <div>
       <H2Title>Filter contacts</H2Title>
